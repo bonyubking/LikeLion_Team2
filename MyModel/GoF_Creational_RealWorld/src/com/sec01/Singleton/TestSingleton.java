866290ce2f2bca038827ad03d74a76f55d0fa596
Happy-Lotus <happylotus145@gmail.com>
2025-05-12 13:03:21 +0900

    0512_1

---
+++ MyModel/GoF_Creational_RealWorld/src/com/sec01/Singleton/TestSingleton.java
@@ -0,0 +1,27 @@
+package com.sec01.Singleton;
+
+import java.util.logging.LogManager;
+import java.io.IOException;
+import java.util.logging.*;
+import java.util.logging.Logger;
+
+public class TestSingleton {
+    public static void main(String[] args) {
+        // new 연산자를 사용하지 않고 객체가 정적으로 생성한 메소드 호출
+//        Logger logger1 = Logger.getInstance();
+//        Logger logger2 = Logger.getInstance();
+//        logger1.log("System started");
+        // System.out.println("Same instance: " + (logger1 == logger2));
+
+        Runtime runtime = Runtime.getRuntime();
+        try{
+            runtime.exec("mspaint.exe");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        LogManager res = LogManager.getLogManager();
+        Logger res_Logger = res.getLogger("com.sec01.Singleton.Logger");
+        System.out.println(res_Logger);
+
+    }
+}
---
+++ MyModel/GoF_Creational_RealWorld/src/com/sec04/Builder/TestBuilder.java
@@ -0,0 +1,16 @@
+package com.sec04.Builder;
+
+// 복잡한 객체의 생성과정을 단계별로 분리함 -> 객체 생성을 여러개로 나눔
+// 빌더 객체 생성 -> 빌더의 메소드 호출 -> 메소드 체이닝 -> build()라는 메소드로 최종 객체를 리턴받음
+public class TestBuilder {
+    public static void main(String[] args) {
+        Report report = new ReportBuilder()
+            .setHeader("=== Report ===")
+            .setBody("Data goes here")
+            .setFooter("End of Report")
+            .build();
+        report.print();
+
+        System.out.println(new StringBuffer().append("abc").append("def"));
+    }
+}
