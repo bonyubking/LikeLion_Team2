438e0eb4cfb94d18c200cf743f4fba5e1ac4a5a5
Happy-Lotus <happylotus145@gmail.com>
2025-05-21 16:32:03 +0900

    0521_2

--- MySpring/SpringJDBC03/src/main/java/com/test01/GoodsVo.java
+++ MySpring/SpringJDBC03/src/main/java/com/test01/GoodsVo.java
@@ -1,5 +1,17 @@
 package com.test01;
 
-public class GoodsVo {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class GoodsVo {
+  private String code;
+  private String name;
+  private int price;
+  private String maker;
 }
