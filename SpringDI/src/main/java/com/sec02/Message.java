50eb76a73e1e30c1360234472ea9b8478c119e44
yeon47 <cerry0724@naver.com>
2025-05-21 17:25:11 +0900

    Remove

--- SpringDI/src/main/java/com/sec02/Message.java
+++
@@ -1,6 +0,0 @@
-package com.sec02;
-
-public interface Message {
-	String getMessage();
-
-}
--- SpringDI/src/main/java/com/sec03/Apple.java
+++
@@ -1,9 +0,0 @@
-package com.sec03;
-
-public class Apple implements Fruit {
-	@Override
-	public String getName() {
-		return "Apple";
-	}
-
-}
